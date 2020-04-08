d5f182d2b944d69e5f84be49d431207646a1b5af
RindiRio95 <a19anjst@student.his.se>
2020-04-08 09:24:16 +0200

    länkade så att man kan gå mellan main activity och second activity och tillbaka

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.hej);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String hej = extras.getString("hej");
+            textView.setText(hej);
+        }
+        Button close = findViewById(R.id.close_second_activity);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
